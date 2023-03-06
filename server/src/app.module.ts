3ae1adb1b531ee468a6e8fa5a0f988243185b665
Gilin3000 <lotus0110@nate.com>
2023-03-06 13:48:44 +0900

    feat : gpt module & config init

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GptModule } from './gpt/gpt.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), GptModule],
   controllers: [AppController],
   providers: [AppService],
 })
